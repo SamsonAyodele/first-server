63518177339163c13a56bbb48933baaaa406ca51
Samson Ayodele <samson.idowu@bankly.ng>
2024-01-17 17:06:15 +0100

    Basics

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+const express = require("express")
+const server = express()
+
+server.get("/", function(req, res){
+  res.send("<h1>Hello World</h1> <br/> <a href='/ayo'>Visit Ayo</a>")
+})
+
+server.get("/ayo", function(req, res){
+  res.send("<h1>Hello Ayo</h1> <br/> <a href='/'>Go back home</a>")
+})
+
+server.listen(3000, function(){
+  console.log("server is listening on port 3000")
+})
